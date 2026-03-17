e92f0ef97f153826fb207400289e4d3a0f0add90
Paul Rooney <paulroon@gmail.com>
2026-03-17 21:26:10 +0000

    update release hooks for pr-symf

--- partrocks-symfony/_resources/_deploy/hooks/instance-bootstrap.sh
+++ partrocks-symfony/_resources/_deploy/hooks/instance-bootstrap.sh
@@ -4,7 +4,7 @@ set -eu
 APP_DIR="${APP_DIR:-/app}"
 cd "$APP_DIR"
 
-echo "[bootstrap] starting runtime bootstrap"
+echo "[bootstrap] starting instance bootstrap"
 
 # Ensure runtime dirs exist with sane permissions
 mkdir -p var/cache var/log
