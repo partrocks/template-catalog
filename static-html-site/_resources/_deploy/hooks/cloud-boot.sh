081f96eed774d6d40c45ba500b7baf6cbf403534
Paul Rooney <paulroon@gmail.com>
2026-04-03 11:15:45 +0100

    cleanup

--- static-html-site/_resources/_deploy/hooks/cloud-boot.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# Archive-based static templates do not run a container; this satisfies environments.yaml boot.script.
-exit 0
--- static-html-site/_resources/app/js/app.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("cta");
-const message = document.getElementById("message");
-
-if (button && message) {
-  button.addEventListener("click", () => {
-    message.textContent = "Hello from your PartRocks static site!";
-  });
-}
